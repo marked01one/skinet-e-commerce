81d357e189ca0215c4a6481a0e61efeb0d7d8692
Khoi <110973167+marked01one@users.noreply.github.com>
2022-12-27 14:41:57 +0700

    API Architecture outlined

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -22,6 +23,10 @@ namespace API
       // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      // .AddScoped(): Create the repository for the duration of the HTTP request
+      // Automatically disposes the controller & repository after the request is finished
+      services.AddScoped<IProductRepository, ProductRepository>();
+
       services.AddControllers();
       services.AddDbContext<StoreContext>(
         x => x.UseSqlite(_config.GetConnectionString("DefaultConnection"))
