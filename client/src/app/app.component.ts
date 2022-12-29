543b55c0ea613813e2515152c595e7211a233e00
Khoi <110973167+marked01one@users.noreply.github.com>
2022-12-29 16:49:23 +0700

    Created a one-line python script to streamline launching development server

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiNET';
+
+  products: any[];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
+      this.products = response.data;
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
---
+++ dev.py
@@ -0,0 +1,18 @@
+import subprocess, webbrowser
+
+API_COMMANDS = ['start', 'dotnet', 'run']
+CLIENT_COMMANDS = ['start', 'ng', 'serve']
+
+def main():
+  # Activate the API server
+  api_process = subprocess.Popen(API_COMMANDS, shell=True, cwd='API')
+  api_process.wait()
+  
+  # Activate the client server
+  client_process = subprocess.Popen(CLIENT_COMMANDS, shell=True, cwd='client')
+  client_process.wait()
+  
+  # Opens https://localhost:4200, Angular's development URL, in your browser
+  webbrowser.open('https://localhost:4200', new=2)
+if __name__ == '__main__':
+  main()
