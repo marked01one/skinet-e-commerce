052e1b2ef8dffbf26c5a4f61b4c9c22974336603
Khoi <110973167+marked01one@users.noreply.github.com>
2023-01-12 21:53:15 +0700

    Implemented request validation for API

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,18 @@ namespace API.Dtos
 {
   public class RegisterDto
   {
+    [Required]
     public string DisplayName { get; set; }
+    
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+    
+    [Required]
+    [RegularExpression(
+      "(?=^.{6,15}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+      ErrorMessage = "Password must be 6-15 characters long, contain at least 1 lowercase letter, 1 uppercase letter, 1 digit and 1 of the following: @$!%*?&"
+    )]
     public string Password { get; set; }   
   }
 }
