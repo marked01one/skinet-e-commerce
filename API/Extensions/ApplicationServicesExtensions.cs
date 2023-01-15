7696f44f2ea77a1310063d3a2fe54f3a123a37eb
Khoi <110973167+marked01one@users.noreply.github.com>
2023-01-15 14:37:31 +0700

    Implemented API user ordering functionalities

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -15,6 +15,8 @@ namespace API.Extensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
       services.AddScoped<ITokenService, TokenService>();
+      services.AddScoped<IOrderServices, OrderService>();
+      services.AddScoped<IUnitOfWork, UnitOfWork>();
       services.AddScoped<IProductRepository, ProductRepository>();
       services.AddScoped<IBasketRepository, BasketRepository>();
       services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -6,6 +6,7 @@ using API.Dtos;
 using AutoMapper;
 using Core.Entities;
 using Core.Entities.Identity;
+using Core.Entities.OrderAggregate;
 
 namespace API.Helpers
 {
@@ -18,9 +19,36 @@ namespace API.Helpers
         .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
         .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
 
-      CreateMap<Address, AddressDto>().ReverseMap();
+      CreateMap<Core.Entities.Identity.Address, AddressDto>().ReverseMap();
       CreateMap<CustomerBasketDto, CustomerBasket>();
       CreateMap<BasketItemDto, BasketItem>();
+      CreateMap<AddressDto, Core.Entities.OrderAggregate.Address>();
+      CreateMap<Order, OrderToReturnDto>()
+        .ForMember(
+          destination => destination.DeliveryMethod, 
+          options => options.MapFrom(source => source.DeliveryMethod.ShortName)
+        )
+        .ForMember(
+          destination => destination.ShippingPrice, 
+          options => options.MapFrom(source => source.DeliveryMethod.Price)
+        );
+      CreateMap<OrderItem, OrderItemDto>()
+        .ForMember(
+          destination => destination.ProductId, 
+          options => options.MapFrom(source => source.ItemOrdered.Id)
+        )
+        .ForMember(
+          destination => destination.ProductName, 
+          options => options.MapFrom(source => source.ItemOrdered.ProductName)
+        )
+        .ForMember(
+          destination => destination.PictureUrl, 
+          options => options.MapFrom(source => source.ItemOrdered.PictureUrl)
+        )
+        .ForMember(
+          destination => destination.PictureUrl, 
+          options => options.MapFrom<OrderItemUrlResolver>()
+        );
     }
   }
 }
